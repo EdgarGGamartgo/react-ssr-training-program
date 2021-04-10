a5e3482ce5300aa21be6081bd24a9f20fc2efad9
EdgarGGamartgo <edgarggamartgo@gmail.com>
2021-04-09 20:16:14 -0500

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-ssr-training-program
 A ReactJS SSR sample project for the Global EPAM Trining Program
+
+# TODO: Rewrite this app using NextJS
---
+++ web/src/components/MoviesCounter.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+import { fetchMovieRequest, addMovie, fetchMovies } from './../redux'
+
+const noMoviesFound = {
+    color: '#FFFFFF',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translateX(-50%) translateY(-50%)',
+    marginTop: '20vh'
+}
+
+const MoviesCounter = ({ className, movies }) => {
+    return (
+        <>
+        {
+            movies.length !== 0 
+            ? <div data-testid="movies" className={className}>
+                {movies.length} movies found
+              </div> 
+            : <div data-testid="movies2" style={noMoviesFound}>
+                NO MOVIES FOUND
+              </div>
+        }
+        </>
+    )
+}
+
+MoviesCounter.propTypes = {
+    className: PropTypes.string.isRequired,
+}
+
+MoviesCounter.defaultProps = {
+    className: ''
+}
+
+const mapStateToProps = state => {
+    return {
+        movies: state.movie.movies
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+       fetchMovie: (show) => dispatch(fetchMovieRequest(show)),
+       createMovie: (movieData) => dispatch(addMovie(movieData)),
+       fetchMoviesRequest: (filters) => dispatch(fetchMovies(filters))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesCounter)
